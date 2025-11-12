c6c2394d462667f42e010f4f74fedc2a70bd83ac
Lesci <Lesci@LESCIEUR>
2025-11-11 18:16:38 -0600

    Agregar archivos de proyecto.

---
+++ PRUEBA/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PRUEBA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSubir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNumNoAcep = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 86F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum.Location = new System.Drawing.Point(533, 134);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(178, 194);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.Text = "0";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(247, 43);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(96, 50);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(428, 44);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(112, 49);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reiniciar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // btnSubir
+            // 
+            this.btnSubir.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnSubir.Location = new System.Drawing.Point(327, 354);
+            this.btnSubir.Name = "btnSubir";
+            this.btnSubir.Size = new System.Drawing.Size(112, 49);
+            this.btnSubir.TabIndex = 3;
+            this.btnSubir.Text = "Subir";
+            this.btnSubir.UseVisualStyleBackColor = false;
+            this.btnSubir.Click += new System.EventHandler(this.btnSubir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(540, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Frutas aceptadas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Frutas declinadas";
+            // 
+            // lblNumNoAcep
+            // 
+            this.lblNumNoAcep.AutoSize = true;
+            this.lblNumNoAcep.Font = new System.Drawing.Font("Microsoft Sans Serif", 86F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumNoAcep.Location = new System.Drawing.Point(80, 134);
+            this.lblNumNoAcep.Name = "lblNumNoAcep";
+            this.lblNumNoAcep.Size = new System.Drawing.Size(178, 194);
+            this.lblNumNoAcep.TabIndex = 5;
+            this.lblNumNoAcep.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNumNoAcep);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubir);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.lblNum);
+            this.Name = "Form1";
+            this.Text = "Contador de frutas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSubir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNumNoAcep;
+    }
+}
+
---
+++ PRUEBA/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO.Ports;
+using MySql.Data.MySqlClient;
+
+namespace PRUEBA
+{
+    public partial class Form1 : Form
+    {
+        string conexionSql = "Server=localhost;Database=contador;port=3306;Uid=root;Pwd=*";
+        private SerialPort ArduinoPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ArduinoPort = new SerialPort();
+            ArduinoPort.PortName = "COM5";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 500;
+            ArduinoPort.WriteTimeout = 500;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+        private void InsertarFrutaAcep(string cantidad, DateTime act)
+        {
+            using (MySqlConnection conect = new MySqlConnection(conexionSql))
+            {
+                conect.Open();
+
+                string insertQ = "INSERT INTO frutaaceptada (numero, fecha_subida) VALUES (@Numero, @Fecha_subida)";
+
+                using (MySqlCommand cmd = new MySqlCommand(insertQ, conect))
+                {
+                    cmd.Parameters.AddWithValue("@Numero", cantidad);
+                    cmd.Parameters.AddWithValue("@Fecha_subida", act);
+                    cmd.ExecuteNonQuery();
+                }
+                conect.Close();
+            }
+        }
+
+        private void InsertarFrutaDen(string cantidad, DateTime act)
+        {
+            using (MySqlConnection conn = new MySqlConnection(conexionSql))
+            {
+                conn.Open();
+                string insertQ = "INSERT INTO frutarechazada(numero, fecha_subida) VALUES(@Numero, @Fecha_subida)";
+
+                using (MySqlCommand cmd = new MySqlCommand(insertQ, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Numero", cantidad) ;
+                    cmd.Parameters.AddWithValue("@Fecha_subida", act) ;
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string dato = ArduinoPort.ReadLine();
+
+                // Actualizar de forma segura desde otro hilo
+                this.Invoke(new Action(() => {
+                    Txt(dato);
+                }));
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores de lectura
+                System.Diagnostics.Debug.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private void Txt(string dato)
+        {
+            // Detectar mensaje de área mayor a 2cm² y actualizar lblNum
+            if (dato.Contains("AREA_MAYOR:1"))
+            {
+                try
+                {
+                    int num = int.Parse(lblNum.Text);
+                    num++;
+                    lblNum.Text = num.ToString();
+
+                    // Opcional: Log para debug
+                    System.Diagnostics.Debug.WriteLine($"Fruta > 2cm² detectada. Total: {num}");
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Error al actualizar contador: " + ex.Message);
+                }
+            }
+
+            // NUEVO: Detectar mensaje de área menor o igual a 2cm² y actualizar lblNumNoAcep
+            if (dato.Contains("AREA_MENOR:1"))
+            {
+                try
+                {
+                    int numNoAcep = int.Parse(lblNumNoAcep.Text);
+                    numNoAcep++;
+                    lblNumNoAcep.Text = numNoAcep.ToString();
+
+                    // Opcional: Log para debug
+                    System.Diagnostics.Debug.WriteLine($"Fruta <= 2cm² detectada. Total: {numNoAcep}");
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Error al actualizar contador no aceptadas: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Intentar encontrar el puerto del Arduino automáticamente
+            string[] puertos = SerialPort.GetPortNames();
+
+            if (puertos.Length == 0)
+            {
+                MessageBox.Show("No se detectaron puertos COM disponibles.\nAsegúrate de que el Arduino está conectado.",
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Mostrar los puertos disponibles y seleccionar
+            string mensaje = "Puertos disponibles:\n" + string.Join("\n", puertos);
+            MessageBox.Show(mensaje, "Puertos COM detectados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Intentar con el primer puerto disponible o COM5
+            string puertoSeleccionado = puertos.Contains("COM5") ? "COM5" : puertos[0];
+
+            try
+            {
+                ArduinoPort.PortName = puertoSeleccionado;
+
+                if (!ArduinoPort.IsOpen)
+                {
+                    ArduinoPort.Open();
+                    lblNum.Text = "0";
+                    MessageBox.Show($"Conectado exitosamente a {puertoSeleccionado}",
+                        "Conexión exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al abrir {puertoSeleccionado}:\n{ex.Message}\n\n" +
+                    "Posibles soluciones:\n" +
+                    "1. Cierra el Monitor Serial del Arduino IDE\n" +
+                    "2. Verifica que el Arduino esté conectado\n" +
+                    "3. Intenta desconectar y reconectar el Arduino",
+                    "Error de conexión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                {
+                    ArduinoPort.Open();
+                    btnConectar.Text = "Desconectar";
+                }
+                else
+                {
+                    ArduinoPort.Close();
+                    btnConectar.Text = "Conectar";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error de conexión",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("E");
+            lblNum.Text = "0";
+        }
+
+        private void btnSubir_Click(object sender, EventArgs e)
+        {
+            string datos = $"Cantidad de frutas aceptadas: {lblNum.Text}\n" +
+                $"Cantidad de frutas rechazadas: {lblNumNoAcep.Text}";
+
+            try
+            {
+                InsertarFrutaAcep(lblNum.Text, DateTime.Now);
+                InsertarFrutaDen(lblNumNoAcep.Text, DateTime.Now);
+
+                MessageBox.Show("Datos guardados exitosamente:\n\n" + datos,
+                    "Información de registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ArduinoPort.Write("E");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
